83242ef34c0550a016459a2b683ca01b4483f3ac
fedir72 <fedir72@ua.fm>
2022-01-18 17:54:53 +0200

    empty app

---
+++ itunesParseJsonAplication/itunesParseJsonAplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  itunesParseJsonAplication
+//
+//  Created by Fedii Ihor on 18.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
